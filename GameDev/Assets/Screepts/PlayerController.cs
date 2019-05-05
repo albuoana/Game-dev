cb71cd9c179406a5ea580decf7880f7f4ed5867d
Oana <oana_albu97@yahoo.ro>
2019-05-05 16:38:36 +0300

    work
    progress

---
+++ GameDev/Assets/Screepts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D theRB;
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode jump;
+    public KeyCode throwBall;
+    public Transform groundCheckPoint;
+    public bool isGrounded;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+
+    void Start()
+    {
+        theRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
+
+        if (Input.GetKey(left))
+        {
+            //velicty ---speed
+            theRB.velocity = new Vector2(-moveSpeed, theRB.velocity.y);
+        }
+        else if (Input.GetKey(right))
+        {
+            theRB.velocity = new Vector2(moveSpeed, theRB.velocity.y);
+        }
+        else
+        {
+            theRB.velocity = new Vector2(0, theRB.velocity.y);
+        }
+
+        if (Input.GetKeyDown(jump) && isGrounded)
+        {
+            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+        }
+    }
+}
