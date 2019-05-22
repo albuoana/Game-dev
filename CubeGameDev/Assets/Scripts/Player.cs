0b8e3c217dbfc3d849afda5c930b710c8ce925e7
Oana <oana_albu97@yahoo.ro>
2019-05-22 23:23:39 +0300

    add 3d small game
    add 3d small game

---
+++ CubeGameDev/Assets/Scripts/Player.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private bool onGround;
+    private float jumpPressure;
+    private float minJump;
+    private float maxJump;
+    private Rigidbody rbody;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        onGround = true;
+        jumpPressure = 0;
+        minJump = 2f;
+        maxJump = 10f;
+        rbody = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (onGround)
+        {
+            if (Input.GetButton("Jump"))
+            {
+                if (jumpPressure <  maxJump)
+                {
+                    jumpPressure += Time.deltaTime * 10f;
+                }
+                else
+                {
+                    jumpPressure = maxJump;
+                }
+                anim.SetFloat("jumpPressure", jumpPressure + minJump);
+                anim.speed = 1f + jumpPressure/10f;
+            }
+            else
+            {
+                //jump
+                if (jumpPressure > 0f)
+                {
+                    jumpPressure = jumpPressure + minJump;
+                    rbody.velocity = new Vector3(jumpPressure/10f, jumpPressure, 0f);
+                    jumpPressure = 0;
+                    onGround = false;
+                    anim.SetFloat("jumpPressure", 0f);
+                    anim.SetBool("onGround", onGround);
+                    anim.speed = 1f;
+                }
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("ground"))
+        {
+            onGround = true;
+            anim.SetBool("onGround", onGround);
+        }
+    }
+}
